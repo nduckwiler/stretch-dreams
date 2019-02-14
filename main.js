54eb4140f302c22f922d252fadac2cfc877622f3
Nick Duckwiler <nduckwiler@gmail.com>
2019-02-14 08:24:26 -0500

    Bound clipping circles to borders of svg

--- main.js
+++ main.js
@@ -70,18 +70,29 @@ window.onload = () => {
           .attr('height', s.height)
           .attr('fill', colors.next());
 
-      // TODO: implement a bounding function for the cx and cy attributes
-      // Should be the range: (radius, width - radius)
       d3.select('defs')
         .append('clipPath')
           .attr('id', 'clip-' + s.level)
         .append('circle')
-          .attr('cx', Math.floor(Math.random() * s.width) - radius)
-          .attr('cy', Math.floor(Math.random() * s.height) - radius)
+          .attr('cx', getRandomInt(radius, s.width - radius))
+          .attr('cy', getRandomInt(radius, s.height - radius))
           .attr('r', radius)
+
     }
   });
 
 
 
 };
+
+/**************
+Helper Functions
+**************/
+
+// Copied from MDN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
